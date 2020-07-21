2e0a9d27a994a5b9bbddc9b8fe429e786c065eea
mumrik <uoon.dev@gmail.com>
2020-07-21 21:25:42 +0900

    init

---
+++ sujin/step01/homework.js
@@ -0,0 +1,33 @@
+// star
+
+
+let star = '';
+let size = 6;
+
+/* 
+let star = '';
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size - i; j++) {
+    if (j === 0 || j === size - 1 - i) {
+      star += '*';
+    } else {
+      if (i === 0) {
+        star += '*';
+      } else {
+        star += ' ';
+      }
+    }
+  }
+  star += '\n';
+}
+*/
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size * 2; j++) {
+    if (size / 2 === 5) {
+    }
+  }
+  star += '\n';
+}
+
+console.log(star);
---
+++ sujin/step01/test01.js
@@ -0,0 +1,33 @@
+// var funcThis = null;
+// function Func() {
+//   funcThis = this;
+// }
+
+// var o1 = Func();
+
+// if (funcThis === window) {
+//   console.log(1);
+// }
+
+// var o2 = new Func();
+
+// if (funcThis === o2) {
+//   console.log(2);
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// var star = '';
+
+// for (var i = 0; i < 5; i++) {
+//   for (var j = 4 - i; j < 5; j++) {
+//     star += '*';
+//   }
+//   star += '\n';
+// }
+
+console.log(star);
