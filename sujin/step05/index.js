345a5e37d24055dc1b26f32f0c060833df6a0824
mumrik <uoon.dev@gmail.com>
2020-08-27 21:15:23 +0900

    modify lotto in sujin

--- sujin/step05/index.js
+++ sujin/step05/index.js
@@ -1,17 +1,37 @@
 const lottoPrice = 1000;
 const lottoNumberLimit = 45;
 const lottoMaxLength = 6;
+let createdLottos;
+let luckyLotto;
+// const buyButton = document.querySelector('.btn-buy');
+// const compareButton = document.querySelector('.btn-compare');
+// const moneyInput = document.querySelector('.input-price');
+// const inputPrice = moneyInput.value;
+let boughtLottoCnt;
+// console.log('@@@@@@inputPrice', inputPrice);
 
-const buyLottos = (inputPrice) => {
+const updateInputPrice = (e) => {
+    const inputPrice = Number(e.target.value);
+    boughtLottoCnt = Math.floor(inputPrice / lottoPrice);
+};
+
+// buildLottos(2);
+const buildLottos = (count) => {
+    // const inputPrice = parseInt(moneyInput.value);
     console.log('buyLotts(inputPrice) >>> ');
-    const boughtLottoCnt = Math.floor(inputPrice / lottoPrice);
+    
 
-    console.log('inputPrice : ' + inputPrice);
-    console.log('boughtLottoCnt : ' + boughtLottoCnt);
-
-    createLottos(boughtLottoCnt);
-    console.log('buyLotts(inputPrice) <<< ');
+    // console.log('inputPrice : ' + inputPrice);
+    // console.log('boughtLottoCnt : ' + boughtLottoCnt);
+    
+    createdLottos = createLottos(count);
+    bindLottos(createdLottos);
+    // console.log('buyLotts(inputPrice) <<< ');
 }
+
+
+// const createdLottos = buyLottos(5000); 
+
 
 const generateRandomNumber = (maxLen) => Math.floor(Math.random() * maxLen); 
 
@@ -35,14 +55,25 @@ const createLottoNumbers = () => {
 const createLottos = (boughtLottoCnt) => {
     console.log('createLottos(boughtLottoCnt) >>> ');
     const lottos = [];
-    let lottoBalls = "";
 
     for (let i = 0; i < boughtLottoCnt; i++){
         lottos.push(createLottoNumbers());
     }
 
-    for (let i = 0; i< lottos.length; i++){
-        lottoBalls += '<span>' + lottos[i] + '</span>';
+    // bindLottos(lottos);
+    
+    console.log('createLottos(boughtLottoCnt) <<< ');
+    return lottos;
+}
+
+const bindLottos = (lottos) => {
+    let lottoBalls = "";
+
+    for (let i = 0; i < lottos.length; i++){
+        for (let j = 0; j < lottoMaxLength; j++) {
+            lottoBalls += '<span>' + lottos[i][j] + '</span> ';
+        }
+        lottoBalls += '<br>';
     }
 
     let lottoContents = document.querySelector('.lotto-contents');
@@ -51,9 +82,6 @@ const createLottos = (boughtLottoCnt) => {
     lottoContents.appendChild(lottoContentObj);
 
     lottoContentObj.innerHTML = lottoBalls;
-    
-    console.log('createLottos(boughtLottoCnt) <<< ');
-    return lottos;
 }
 
 const setLuckyNumbers = () => {
@@ -74,35 +102,59 @@ const setLuckyNumbers = () => {
     return luckyNumbers;
 }
 
-const compareLottos = () => {
-    console.log('compareLottos() >>> ');
-    
-    const boughtLottoNumbers = [];
-    const winNumbers = [];
-    let equalNumbers = 0;
+const compareLottos = (createdLottos, luckyLotto) => {
+    const wins = [];
+    createdLottos = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [44, 7, 12, 55, 123, 1232]]
+    luckyLotto = [4, 5, 6, 7, 8, 9];
+    createdLottos.forEach(createdLotto => {
+        let win = 0; // [3, 3, 1]
 
-    createLottos(boughtLottoCnt);
-    setLuckyNumbers();
-
-    boughtLottoNumbers.push(createLottos(boughtLottoCnt));
-    winNumbers.push(setLuckyNumbers());
-
-    console.log('boughtLottoNumbers : ' + boughtLottoNumbers);
-    console.log('boughtLottoNumbers.length : ' + boughtLottoNumbers.length);
-    console.log('winNumbers : ' + winNumbers);
-    console.log('winNumbers.length : ' + winNumbers.length);
-
-    for (let i = 0; i < boughtLottoNumbers.length; i++){
-        for (let j = 0; j < winNumbers.length; j++){
-            if(boughtLottoNumbers[i]===winNumbers[j]){gg
-                equalNumbers++;
+        createdLotto.forEach(lottoNumber => {
+            if (luckyLotto.contains(lottoNumber)) {
+                win++;
             }
-        }
-    }
-    console.log('equalNumbers : ' + equalNumbers);
+        })
+        wins.push(win);
+    })
 
-    console.log('compareLottos() <<< ');
-    return equalNumbers;
+    console.log(wins);
+
+    const filteredWins3 = wins.filter(winCount => winCount === 3).length;
+    const filteredWins4 = wins.filter(winCount => winCount === 4).length;
+    const filteredWins5 = wins.filter(winCount => winCount === 5).length;
+    const filteredWins6 = wins.filter(winCount => winCount === 6).length;
+
+    console.log(filteredWins);
+    // console.log('compareLottos() >>> ');
+    
+    // const boughtLottoCnt = 2;
+    // let boughtLottoNumbers = [];
+    // let winNumbers = [];
+    // let equalNumbers = 0;
+
+    // createLottos(boughtLottoCnt);
+    // setLuckyNumbers();
+
+    // boughtLottoNumbers = createLottos(boughtLottoCnt);
+    // // winNumbers.push(setLuckyNumbers());
+    // winNumbers = setLuckyNumbers;
+
+    // console.log('boughtLottoNumbers : ' + boughtLottoNumbers);
+    // console.log('boughtLottoNumbers.length : ' + boughtLottoNumbers.length);
+    // console.log('winNumbers : ' + winNumbers);
+    // console.log('winNumbers.length : ' + winNumbers.length);
+
+    // for (let i = 0; i < boughtLottoNumbers.length; i++){
+    //     for (let j = 0; j < winNumbers.length; j++){
+    //         if(boughtLottoNumbers[i]===winNumbers[j]){gg
+    //             equalNumbers++;
+    //         }
+    //     }
+    // }
+    // console.log('equalNumbers : ' + equalNumbers);
+
+    // console.log('compareLottos() <<< ');
+    // return equalNumbers;
 }
 
 const displayResult = () => {
@@ -110,9 +162,19 @@ const displayResult = () => {
     console.log('displayResult() <<< ');
 }
 
-console.log(' end) buyLottos(2000) : ' + buyLottos(5000));
-console.log(' end) createLottos(2) : ' + createLottos(5));
-console.log(' end) createLottoNumbers() : ' + createLottoNumbers());
+// console.log(' end) buyLottos(2000) : ' + buyLottos(5000));
+// console.log(' end) createLottos(2) : ' + createLottos(5));
+// console.log(' end) createLottoNumbers() : ' + createLottoNumbers());
 console.log(' end) compareLottos() : ' + compareLottos());
-console.log(' end) displayResult() : ' + displayResult());
+// console.log(' end) displayResult() : ' + displayResult());
 
+const initialize = () => {
+    luckyLotto = setLuckyNumbers();
+    buyButton.addEventListener('click', () => buildLottos(boughtLottoCnt));
+    compareButton.addEventListener('click', () => compareLottos(createdLottos, luckyLotto));
+    moneyInput.addEventListener('input', updateInputPrice)
+}
+
+// initialize();
+
+compareLottos();
